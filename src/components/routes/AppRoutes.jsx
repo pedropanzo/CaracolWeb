1299b06a92749c20df8875055ecea804e2a851b4
Elias Bernardo <bernardomoncano@gmail.com>
2026-04-01 14:53:21 -0100

    Suspensao de alunos

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -11,6 +11,7 @@ import StudentsAcademico from '../pages/students/StudentsAcademico';
 import StudentConfirm from '../pages/students/StudentConfirm';
 import StudentPage from '../pages/students/StudentPage';
 import StudentDropOut from '../pages/students/StudentDropOut';
+import SuspensionPage from '../pages/students/SuspensionPage';
 import SchoolForm from '../pages/school/SchoolForm';
 import SchoolList from '../pages/school/SchoolList';
 import AcademicYearPage from '../pages/academicYear/AcademicYear';
@@ -44,6 +45,7 @@ export default function AppRoutes() {
           <Route path="/alunos/novo" element={<StudentForm />} />
           <Route path="/alunos/dados-pessoal" element={<StudentPage />} />
           <Route path="/alunos/desistente" element={<StudentDropOut />} />
+          <Route path='/alunos/suspensao' element={<SuspensionPage />} />
           <Route path="/escolas/nova" element={<SchoolForm />} />
           <Route path="/escolas" element={<SchoolList />} />
           <Route path="/ano-lectivo" element={<AcademicYearPage />} />

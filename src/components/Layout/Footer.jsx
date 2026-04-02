9fbbdc4c6b4307fe30d5031fd50604d1ea6deb55
Pedro Panzo <pedropanzo53@gmail.com>
2026-04-01 21:45:28 -0300

    Render fixed

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (<>
+    <div className="footer">
+      <div>
+        <strong>Copyright</strong> Wobotec &copy; 2020-{new Date().getFullYear()}
+      </div>
+    </div>
+  </>);
+}
+
+export default Footer;

1b9e346a0f83e5affa46d76929b44a7f2c3b0d12
Pedro Panzo <pedropanzo53@gmail.com>
2026-04-03 23:49:23 -0300

    fixed lockcreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
 import  AppRoutes  from './components/routes/AppRoutes.jsx'
-import $ from 'jquery';
 
 import './assets/css/bootstrap.min.css';
 import './assets/font-awesome/css/font-awesome.css'
